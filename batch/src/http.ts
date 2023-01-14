6a8b22b85ec7b9230162a2ad802b43e03aed513a
fourside <fourside@gmail.com>
2023-01-14 12:10:33 +0900

    remove cli-progress

--- batch/src/http.ts
+++ batch/src/http.ts
@@ -1,22 +1,9 @@
-import cliProgress from "cli-progress";
-import { Env } from "./env";
 import { sleep } from "./sleep";
 import { ItemData, ImageData } from "./type";
 
 export async function fetchImages(items: ItemData[]): Promise<ImageData[]> {
   const imageDataList: ImageData[] = [];
-  const progressBar = new cliProgress.SingleBar({
-    format: ` {bar} {percentage}% | {value}/{total}`,
-    barCompleteChar: "\u2588",
-    barIncompleteChar: "\u2591",
-    hideCursor: true,
-  });
-  if (!Env.production) {
-    progressBar.setTotal(items.length);
-    progressBar.start(items.length, 0);
-  }
 
-  let count = 0;
   for (const item of items) {
     const res = await fetch(item.imageUrl);
     const arrayBuffer = await res.arrayBuffer();
@@ -25,9 +12,6 @@ export async function fetchImages(items: ItemData[]): Promise<ImageData[]> {
       itemId: item.id,
       image,
     });
-    if (!Env.production) {
-      progressBar.update(++count);
-    }
     await sleep(2000);
   }
   return imageDataList;
